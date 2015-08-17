61f757b3eccd677e4c58db9ecba64cf31ed5eff2
Ankit Anand <way2android@gmail.com>
2015-08-18 02:20:41 +0530

    Update knct.js

--- knct.js
+++ knct.js
@@ -85,6 +85,30 @@
                     }
                 ]
             }, {
+                "name": "Android Marshmallow",
+                "wallpaper": [{
+                        "author": "Ankit Anand",
+                        "url": "https://googledrive.com/host/0B1iXjlfkhRKFTU5PSWRETTRkZlk",
+                        "thumbUrl": "https://googledrive.com/host/0B1iXjlfkhRKFSFBXMXA3LVdlNHM",
+                        "name": "Wall-1"
+                    }, {
+                        "author": "Ankit Anand",
+                        "url": "https://googledrive.com/host/0B1iXjlfkhRKFLTRhUlVlYjAtR3M",
+                        "thumbUrl": "https://googledrive.com/host/0B1iXjlfkhRKFYVVvWUFndlJRQlU",
+                        "name": "Wall-2"
+                    }, {
+                        "author": "Ankit Anand",
+                        "url": "https://googledrive.com/host/0B1iXjlfkhRKFZ1JaVlhZUEdZTzg",
+                        "thumbUrl": "https://googledrive.com/host/0B1iXjlfkhRKFY0d4dkJCZDJKTUE",
+                        "name": "Wall-3"
+                    }, {
+                        "author": "Ankit Anand",
+                        "url": "https://googledrive.com/host/0B1iXjlfkhRKFMW0zVWhtaUxELXM",
+                        "thumbUrl": "https://googledrive.com/host/0B1iXjlfkhRKFVkl2V0JuNTladEU",
+                        "name": "Wall-4"
+                    }
+                ]
+            }, {
                 "name": "Black & White",
                 "wallpaper": [{
                         "author": "Ankit Anand",
